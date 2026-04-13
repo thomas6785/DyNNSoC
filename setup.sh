c0f4553d5b244dfe0fb3d8f19286852e4bf17c1b
Thomas O'Dea <thomaseodea@gmail.com>
2026-04-13 19:55:17 +0100

    add RISCV toolchain to PATH

--- setup.sh
+++ setup.sh
@@ -3,5 +3,8 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export SOCROOT=$SCRIPT_DIR
 
+# Add Vivado to PATH
 PATH=$PATH:/tools/Xilinx/2025.1/Vivado/bin/
 
+# Add RISCV tools to PATH
+PATH=$PATH:/home/tudentstudent/riscv/bin/
