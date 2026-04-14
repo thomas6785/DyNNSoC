fe23b42a2c09f559cd04b419abc7835d16fa36e2
Thomas O'Dea <thomaseodea@gmail.com>
2026-04-14 22:02:10 +0100

    create stub of MVU to speed up elab time

---
+++ rtl/stubs/mvutop_wrapper_STUB.sv
@@ -0,0 +1,17 @@
+`timescale 1ns/1ps
+
+module mvutop_wrapper (
+    input logic HCLK,
+    input logic HRESETn,
+    output logic irq_flag,
+    ahb_intf_s.slave AHB_IF
+);
+    always @ (posedge HCLK) begin
+        if (AHB_IF.HSEL && AHB_IF.HTRANS[1] && ~HRESETn) begin
+            $error("Unexpectedly got transaction to MVU top wrapper when stub version was instantiated!");
+        end
+    end
+    assign AHB_IF.HRESP = 1'b1; // error all transactions
+    assign AHB_IF.HREADYOUT = 1'b1; // always ready
+    assign AHB_IF.HRDATA = 32'b0; // return zeros on reads
+endmodule
