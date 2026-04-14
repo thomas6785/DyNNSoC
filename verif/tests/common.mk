6aca1619985e1f0d520cf2ee73096dfbf6382a74
Thomas O'Dea <thomaseodea@gmail.com>
2026-04-14 13:18:21 +0100

    rename targets

--- verif/tests/common.mk
+++ verif/tests/common.mk
@@ -1,42 +1,43 @@
-run: artefacts/main.hex compile-sv elab-sv sim-sv report
+run: newlog artefacts/main.hex comp elab sim report
 
 FW_DIR  := $(SOCROOT)/firmware
 include $(FW_DIR)/firmware.mk
 
 .SILENT: artefacts/log.log
-artefacts/log.log:
+.PHONY: newlog
+newlog:
 	cd artefacts &&\
 	echo "" > log.log
 
-.PHONY: compile-sv elab-sv sim-sv report clean
-.SILENT: compile-sv elab-sv sim-sv report
+.PHONY: comp elab sim report clean
+.SILENT: comp elab sim report
 
 # There are separate targets for compile, elab, sim
 # They aren't actually dependencies since the user might want to rerun just one,
 # but Make can detect if they are out of date so it will always rerun them all
 # if you want a full clean run use "make run"
 
-compile-sv: artefacts/log.log
+comp: newlog
 	echo "\nCompiling test $$(pwd)"								&&\
 	cd artefacts													&&\
-	xvlog -f ../filelist 						>> log.log
+	xvlog -sv -f ../filelist 						>> log.log
 
-elab-sv: artefacts/log.log
+elab: newlog
 	echo "\nElaborating test $$(pwd)"								&&\
 	cd artefacts													&&\
 	xelab -debug typical TB_toplevel -s tb_sim	>> log.log
 
-sim-sv: artefacts/log.log
+sim: newlog
 	echo "\nSimulating test $$(pwd)"								&&\
 	cd artefacts													&&\
 	xsim tb_sim -runall 						>> log.log
 
-report: sim-sv artefacts/log.log
+report: sim
 	cd artefacts													&&\
 	tail log.log
 
-gui: compile-sv elab-sv
-	cd artefacts &&\
+gui:
+	cd artefacts													&&\
 	xsim tb_sim -gui
 
 clean:
